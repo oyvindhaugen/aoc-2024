9462467e0f777f336724b2624844e7d0e1478ee1
Øyvind Haugen <oyvindet@gmail.com>
2024-10-29 18:20:46 +0100

    added some utility functions

--- src/utils.ts
+++ src/utils.ts
@@ -12,3 +12,18 @@ export const getTodayLines = async (year: number, day: number) => {
   const text = await res.text();
   return text.split('\n');
 };
+
+export const getNumbersFromString = (input: string): number[] =>
+  (input.match(/\d+/g) || []).map(Number);
+
+export const numberToBinary = (n: number, length: number = 8): string =>
+  n.toString(2).padStart(length, '0');
+
+export const distanceBetweenPointsGrid = (
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+): number => Math.abs(x1 - x2) + Math.abs(y1 - y2);
+
+export const binaryToDecimal = (binaryString: string): number => parseInt(binaryString, 2);
