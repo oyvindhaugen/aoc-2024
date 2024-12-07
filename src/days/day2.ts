d3e48d0f81170ef55ac2e4449e76cd16f20559ca
Arie Haugen <oyvindet@gmail.com>
2024-12-07 21:56:51 +0100

    day 2 completed

--- src/days/day2.ts
+++ src/days/day2.ts
@@ -1,25 +1,38 @@
 export const part1 = (lines: string): string | number => {
     const splitLines = lines.split('\n').filter(x => x).map(line => line.split(' ').map(s => parseInt(s)));
 
-    const filteredSplitLines = splitLines.filter(line => { 
-        const isIncreasing = line.every((num, i, arr) => {
-            if (i === 0) return true; 
-            const diff = num - arr[i - 1];
-            return diff >= 1 && diff <= 3;
-        });
-    
-        const isDecreasing = line.every((num, i, arr) => {
-            if (i === 0) return true; 
-            const diff = arr[i - 1] - num;
-            return diff >= 1 && diff <= 3;
-        });
-
-        return isIncreasing || isDecreasing
-    });
-
-    return filteredSplitLines.length;
+    return splitLines.filter(line => isIncreasingOrDecreasing(line)).length;
 };
 
 export const part2 = (lines: string): string | number => {
-    return 0;
+    const splitLines = lines.split('\n').filter(x => x).map(line => line.split(' ').map(s => parseInt(s)));
+
+    return splitLines.filter(line => { 
+        const isValid = isIncreasingOrDecreasing(line);
+        let isDampenedValid = false;
+        if (!isValid) {
+            line.forEach((_, idx) => {
+                const lineToCheck = [...line.slice(0, idx), ...line.slice(idx + 1)];
+                if (isIncreasingOrDecreasing(lineToCheck)) isDampenedValid = true;
+            })
+        }
+
+        return isValid || isDampenedValid
+    }).length;
 };
+
+const isIncreasingOrDecreasing = (line: number[]): boolean => {
+    const isIncreasing = line.every((num, i, arr) => {
+        if (i === 0) return true; 
+        const diff = num - arr[i - 1];
+        return diff >= 1 && diff <= 3;
+    });
+
+    const isDecreasing = line.every((num, i, arr) => {
+        if (i === 0) return true; 
+        const diff = arr[i - 1] - num;
+        return diff >= 1 && diff <= 3;
+    });
+
+    return isIncreasing || isDecreasing
+}
