6dfff1b9738de207f79309a03b704751193d689a
oyvindhaugen <oyvindet@gmail.com>
2024-12-13 12:09:57 +0100

    added timer

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import {part1, part2} from './days/day6.ts';
 import dotenv from 'dotenv';
 
 const main = async (year: number, day: number) => {
+  console.time()
   dotenv.config();
   const lines: string = await getTodayLines(day, year);
   const answer1 = part1(lines);
   const answer2 = part2(lines);
 
   console.log('part 1: ' + answer1 + '\npart 2: ' + answer2);
+  console.timeEnd();
+
 };
 
 const [year, day] = process.argv.slice(2);
